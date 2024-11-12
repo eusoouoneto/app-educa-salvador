a99845c0893f0b1bdd88d729f4a5e07f7f5cec49
Eraldo <neto_oliveira12017@outlook.com>
2024-11-12 16:15:04 -0300

    atualizando

---
+++ script.js
@@ -0,0 +1,24 @@
+//function avaliacaoEscolas() {
+  //  alert("Abrindo a Avaliação de Escolas...");
+    // Código para redirecionar ou abrir o recurso de Avaliação de Escolas
+//}
+
+//function escolasNaRegiao() {
+    //alert("Abrindo Escolas na Região...");
+    // Código para redirecionar ou abrir o recurso de Escolas na Região
+//}
+
+//function consultarLeis() {
+    //alert("Abrindo Consultar Leis...");
+    // Código para redirecionar ou abrir o recurso de Consultar Leis
+//}
+
+//function solicitarFiscalizacao() {
+    //alert("Abrindo Solicitar Fiscalização...");
+    // Código para redirecionar ou abrir o recurso de Solicitar Fiscalização
+//}
+
+function contato() {
+  alert("Abrindo Contato...");
+  // Código para redirecionar ou abrir o recurso de Contato
+}
